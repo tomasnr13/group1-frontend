3eae1b1d0518c37fb161328e4a25e8cd0600eefc
tomasnr13 <up201906743@fe.up.pt>
2023-04-13 17:41:16 +0100

    added ranking request

--- actividad-frontend/src/components/ranking/index.jsx
+++ actividad-frontend/src/components/ranking/index.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 /**
  * Este componente es de ejemplo y solo sirve para marcar sitios en el layout.
  */
-const fetchProducts = async () => {
+const fetchProducts = async ({}) => {
     try {
         console.log("here")
         const response = await fetch('/products');
@@ -17,9 +17,27 @@ const fetchProducts = async () => {
     }
 };
 
+const fetchRanking = async ({type, fromDate, toDate}) => {
+    try {
+        const response = await fetch('/ranking?type='+type+'&fromDate='+fromDate+'&toDate='+toDate);
+        const data = await response.json();
+        console.log("data ", data);
+        return data; // Assuming you want to display the total confirmed cases globally
+    } catch (error) {
+        console.error("Error fetching data: ", error);
+        return null;
+    }
+};
+
 export const Ranking = ({ width, height }) => {
     //fetch the data from the API
     const [products, setProducts] = useState(null);
+    const [ranking, setRanking] = useState(null);
+    const [type, setType] = useState(null);
+    const [fromDate, setFromDate] = useState(null);
+    const [toDate, setToDate] = useState(null);
+
+
     useEffect(() => {
         const fetchData = async () => {
             const products = await fetchProducts();
@@ -27,7 +45,14 @@ export const Ranking = ({ width, height }) => {
         };
         fetchData();
     }, []);
-    console.log(products);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const ranking = await fetchRanking({type,fromDate,toDate});
+            setRanking(ranking);
+        };
+        fetchData();
+    }, []);
     return (
         <div className="ranking" style={{ width, height }}>
             <div className="tab"></div>
